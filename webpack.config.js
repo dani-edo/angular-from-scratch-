bedcffd281b83f0bb03f6f7ba02db3872e8f1a77
Dani Edo <dani.nur.afandi@gmail.com>
2021-05-17 23:34:04 +0700

    Go on with Angular

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const ScriptExtPlugin = require("script-ext-html-webpack-plugin");
+const { AngularWebpackPlugin } = require("@ngtools/webpack");
+const path = require("path");
 
 module.exports = function () {
   return {
@@ -8,7 +11,12 @@ module.exports = function () {
       path: __dirname + "/dist",
       filename: "app.js",
     },
-
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [{ test: /\.ts$/, loader: "@ngtools/webpack" }],
+    },
     plugins: [
       new CopyWebpackPlugin({
         patterns: [{ from: "src/assets", to: "assets" }],
@@ -17,6 +25,16 @@ module.exports = function () {
         template: __dirname + "/src/index.html",
         output: __dirname + "/dist",
         inject: "head",
+      }),
+      // // commented for expriment :start
+      // new ScriptExtPlugin({
+      //   defaultAttribute: "defer",
+      // }),
+      // // commented for expriment :end
+      new AngularWebpackPlugin({
+        tsConfigPath: "./tsconfig.json",
+        entryModule: path.join(__dirname, "src/app/app.module#AppModule"),
+        sourceMap: true,
       }),
     ],
   };
